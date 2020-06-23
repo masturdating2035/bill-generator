1c6328439c6579d3ae4306b864ad03118586ceee
Masturdating <aligodosi@gmail.com>
2020-06-23 17:05:47 +0430

    fix some bugs

--- js/app.js
+++ js/app.js
@@ -1,33 +1,64 @@
-const foodFee = document.getElementById('foodFee')
-const foodCount = document.getElementById('foodCount')
-const plus = document.getElementById('plus')
-const minus = document.getElementById('minus')
-let foodPrice = document.getElementById('foodPrice')
+const foods = [{
+    id: 1,
+    name: 'قیمه مکزیکی با سس آلفردو',
+    fee: 1000,
+    number: 0,
+    price: 0,
+},
+{
+    id: 2,
+    name: 'قیمه با عدسی',
+    fee: 1000,
+    number: 0,
+    price: 0
+},
+{
+    id: 3,
+    name: 'قیمه نذری محرم',
+    fee: 1000,
+    number: 0,
+    price: 0
+},
+{
+    id: 4,
+    name: 'قیمه ننه بزرگم',
+    fee: 1000,
+    number: 0,
+    price: 0
+}
+]
 
-foodCount.textContent = "0"
-foodPrice.textContent = "0";
+const billGenerator = (foods) => {
+foods.forEach(food => {
+    $('#items').append(`
+    <div class="foods">
+                            
+    <div class="foods-info">
+       <div class="foods-info-img">
+           <img src="./img/dish.jpg">
+       </div>
 
-plus.addEventListener('click', e => {
-    if (foodCount.textContent == 9) {
-        return
-    } else {
-        foodCount.textContent++
-    }
+       <div class="foods-info-details">
+           <p>${food.name}</p>
+           <p><span class="foodFee">${food.fee}</span> تومان</p>
+           <div class="foods-info-details-number">
+               <span class="foodCount">${food.number}</span>
+               <div class="foods-info-details-number-add">
+                   <a href="#" class="plus"><i class="fas fa-plus"></i></a>
+                   <a href="#" class="minus"><i class="fas fa-minus"></i></a>
+               </div>
+           </div>
+       </div>
+   </div> 
 
-    let multipleFood = parseInt(foodCount.textContent) * parseInt(foodFee.textContent)
-
-    foodPrice.textContent = multipleFood
+   <div class="foods-price">
+       <div><span class="foodPrice">${food.price}</span> تومان</div>
+   </div>
+</div>
+<hr>
+    `)
 
 })
+}
 
-minus.addEventListener('click', e => {
-    if (foodCount.textContent == 0) {
-        return
-    } else {
-        foodCount.textContent--
-    }
-
-    let multipleFood = parseInt(foodCount.textContent) * parseInt(foodFee.textContent)
-
-    foodPrice.textContent = multipleFood
-})
+billGenerator(foods)
